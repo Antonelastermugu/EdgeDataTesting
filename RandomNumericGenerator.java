ee18c5529a8b7095b0311304e45e19aafb86270b
Antonelastermugu <118736457+Antonelastermugu@users.noreply.github.com>
2024-03-28 14:33:46 +0100

    Update RandomNumericGenerator.java

--- RandomNumericGenerator.java
+++ RandomNumericGenerator.java
@@ -1,66 +1,67 @@
 package EdgeData.EdgeDataTesting;
 
+
 import java.util.Random;
 
 public class RandomNumericGenerator {
-	// krijon vlere numerike me numer te percaktuar karakteresh per te testuar limitin e nje fushe
-	public static String generateBoundedNumericValue(int numberOfCharacters) {
-		if (numberOfCharacters <= 0) {
-			throw new IllegalArgumentException("Numbers of charachters must be greater than 0");
-		}
-		Random random = new Random();
-		StringBuilder sb = new StringBuilder();
-
-		for (int i = 0; i < numberOfCharacters; i++) {
-			sb.append(random.nextInt(10));
-		}
-		return sb.toString();
-	}
-
-	// krijon vlera mbi ose nen vleren e lejuar ne baze te tipit te te dhenave (int,double, float)
-	public static String generateOverflowUnderflow(String type, String MinOrMax) {
-		switch (type) {
-		case "Integer":
-
-			if (MinOrMax.equals("Min")) {
-				// Integer underflow
-				int minValue = Integer.MIN_VALUE;
-				int underflowValue = minValue - 1;
-				return String.valueOf(underflowValue);
-
-			} else {
-				// Integer overflow
-				int maxValue = Integer.MAX_VALUE;
-				int overflowValue = maxValue + 1;
-				return String.valueOf(overflowValue);
+	//This method creates numerical value with a defined number of characters to test the limit of a numerical field
+		public static String generateBoundedNumericValue(int numberOfCharacters) {
+			if (numberOfCharacters <= 0) {
+				throw new IllegalArgumentException("Numbers of charachters must be greater than 0");
 			}
+			Random random = new Random();
+			StringBuilder sb = new StringBuilder();
 
-		case "Double":
-			if (MinOrMax.equals("Max")) {
-				double maxValue1 = Double.MAX_VALUE;
-				double overflowValue1 = maxValue1 + 1;
-				return String.valueOf(overflowValue1);
-			} else {
-
-				double minValue1 = Double.MIN_VALUE;
-				double underflowValue1 = minValue1 - 1;
-				return String.valueOf(underflowValue1);
+			for (int i = 0; i < numberOfCharacters; i++) {
+				sb.append(random.nextInt(10));
 			}
-
-		case "Float":
-			if (MinOrMax.equals("Max")) {
-				float maxValue2 = Float.MAX_VALUE;
-				float overflowValue2 = maxValue2 + 1;
-				return String.valueOf(overflowValue2);
-			} else {
-				float minValue2 = Float.MIN_VALUE;
-				float underflowValue2 = minValue2 - 1;
-				return String.valueOf(underflowValue2);
-			}
-		default:
-			return "Type of data is not selected";
+			return sb.toString();
 		}
-	}
-	
-	
+
+		//This method creates overflow or underflow values on the datatype (int,double, float)
+		public static String generateOverflowUnderflow(String type, String MinOrMax) {
+			switch (type) {
+			case "Integer":
+
+				if (MinOrMax.equals("Min")) {
+					// Integer underflow
+					int minValue = Integer.MIN_VALUE;
+					int underflowValue = minValue - 1;
+					return String.valueOf(underflowValue);
+
+				} else {
+					// Integer overflow
+					int maxValue = Integer.MAX_VALUE;
+					int overflowValue = maxValue + 1;
+					return String.valueOf(overflowValue);
+				}
+
+			case "Double":
+				if (MinOrMax.equals("Max")) {
+					double maxValue1 = Double.MAX_VALUE;
+					double overflowValue1 = maxValue1 + 1;
+					return String.valueOf(overflowValue1);
+				} else {
+
+					double minValue1 = Double.MIN_VALUE;
+					double underflowValue1 = minValue1 - 1;
+					return String.valueOf(underflowValue1);
+				}
+
+			case "Float":
+				if (MinOrMax.equals("Max")) {
+					float maxValue2 = Float.MAX_VALUE;
+					float overflowValue2 = maxValue2 + 1;
+					return String.valueOf(overflowValue2);
+				} else {
+					float minValue2 = Float.MIN_VALUE;
+					float underflowValue2 = minValue2 - 1;
+					return String.valueOf(underflowValue2);
+				}
+			default:
+				return "Type of data is not selected";
+			}
+		}
+		
+		
 }
