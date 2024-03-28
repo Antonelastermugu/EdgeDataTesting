a8852f7e1cc46247925cb9657b14d62fc833b257
Antonelastermugu <118736457+Antonelastermugu@users.noreply.github.com>
2024-03-28 14:33:22 +0100

    Update RandomAlphabeticStringGenerator.java

--- RandomAlphabeticStringGenerator.java
+++ RandomAlphabeticStringGenerator.java
@@ -1,20 +1,73 @@
 package EdgeData.EdgeDataTesting;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class RandomAlphabeticStringGenerator {
-	private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	private static final String AlphabeticCHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	private static final String AlphabeticNumericCHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	private static final String AlphabeticNumericSpecialCHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-=+";
 
-	// te dhena me nje numer te larte karakteresh
-	public static String StringWithLargeNumerOfCharachters(int length) {
+	public static String AlphabeticString(int length) {
+		// This method generates a string type value with defined number of alphabetical
+		// characters
 		Random random = new Random();
 		StringBuilder sb = new StringBuilder(length);
 
 		for (int i = 0; i < length; i++) {
-			int randomIndex = random.nextInt(CHARACTERS.length());
-			char randomChar = CHARACTERS.charAt(randomIndex);
+			int randomIndex = random.nextInt(AlphabeticCHARACTERS.length());
+			char randomChar = AlphabeticCHARACTERS.charAt(randomIndex);
 			sb.append(randomChar);
 		}
 		return sb.toString();
+	}
+
+	public static String AlphanumericString(int length) {
+		// This method generates a string type value with defined number of characters
+		// including alphabetic and numerical
+		Random random = new Random();
+		StringBuilder sb = new StringBuilder(length);
+
+		for (int i = 0; i < length; i++) {
+			int randomIndex = random.nextInt(AlphabeticNumericCHARACTERS.length());
+			char randomChar = AlphabeticNumericCHARACTERS.charAt(randomIndex);
+			sb.append(randomChar);
+		}
+		return sb.toString();
+	}
+
+	public static String AlphanumericSpecialCharactesString(int length) {
+		// This method generates a string type value with defined number of characters
+		// including alphabetic, numerical and special characters
+		Random random = new Random();
+		StringBuilder sb = new StringBuilder(length);
+
+		for (int i = 0; i < length; i++) {
+			int randomIndex = random.nextInt(AlphabeticNumericSpecialCHARACTERS.length());
+			char randomChar = AlphabeticNumericSpecialCHARACTERS.charAt(randomIndex);
+			sb.append(randomChar);
+		}
+		return sb.toString();
+	}
+	//This method generates different String cases
+	public static List<String> generateDifferentStringCases(String word){
+		List<String> cases = new ArrayList<>();
+		generateCases(word.toCharArray(), 0, cases);
+		return cases;
+	}
+	
+	private static void generateCases(char[] chars, int index, List<String> cases) {
+		if (index == chars.length) {
+			cases.add(String.valueOf(chars));
+			return;
+		}
+		generateCases(chars, index + 1, cases);
+		if (Character.isLetter(chars[index])) {
+			chars[index] = Character.toUpperCase(chars[index]);
+			generateCases(chars, index + 1, cases);
+			chars[index] = Character.toLowerCase(chars[index]);
+		}
 	}
 }
